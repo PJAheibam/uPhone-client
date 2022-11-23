9d53f8e30208988dafb00808706e531ea7a518c1
PJAheibam <prosenjitsingha28@gmail.com>
2022-11-23 21:42:51 +0600

    containers created

---
+++ src/components/containers/index.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { breakpoint } from "../../utils/breakpoints";
+
+export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  gap: 3ram;
+  min-height: calc(100vh - 55px);
+  max-width: ${breakpoint.xxl + "px"};
+  margin-inline: auto;
+`;
