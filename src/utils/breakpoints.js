70e97fbbffee455c2492f8c9ecdfdb64b8bdea1e
PJAheibam <prosenjitsingha28@gmail.com>
2022-11-23 21:38:04 +0600

    breakpoints added

---
+++ src/utils/breakpoints.js
@@ -0,0 +1,15 @@
+export const breakpoint = {
+  sm: 600,
+  md: 900,
+  lg: 1024,
+  xl: 1280,
+  xxl: 1366,
+};
+
+export const device = {
+  sm: `screen and (min-width: ${breakpoint.sm}px)`,
+  md: `screen and (min-width: ${breakpoint.md}px)`,
+  lg: `screen and (min-width: ${breakpoint.lg}px)`,
+  xl: `screen and (min-width: ${breakpoint.xl}px)`,
+  xxl: `screen and (min-width: ${breakpoint.xxl}px)`,
+};
