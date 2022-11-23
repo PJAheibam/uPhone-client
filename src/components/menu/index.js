01f584b7b8305093213b569247d0ab7935619913
PJAheibam <prosenjitsingha28@gmail.com>
2022-11-23 21:43:11 +0600

    menu created

---
+++ src/components/menu/index.js
@@ -0,0 +1,39 @@
+import { animated } from "react-spring";
+import styled from "styled-components";
+
+export const MenuBtnContainer = styled.div`
+  position: relative;
+  width: fit-content;
+  height: fit-content;
+`;
+
+export const MenuContainer = styled(animated.div)`
+  position: absolute;
+  top: 110%;
+  right: 0;
+  border-radius: var(--border-radius-md);
+  overflow: hidden;
+  background-image: var(--paper-3);
+  box-shadow: var(--box-shadow-sm);
+  border-top: 1px solid hsl(var(--outline-variant));
+  border-left: 1px solid hsl(var(--outline-variant));
+`;
+
+export const MenuItem = styled.button`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  gap: 1rem;
+  font-size: 1.15rem;
+  padding: 0.35em 1em;
+  color: hsl(var(--text-secondary));
+  background-image: inherit;
+  &:hover {
+    background-image: var(--paper-5);
+  }
+`;
+
+export const MenuText = styled.span`
+  text-transform: capitalize;
+  font-size: inherit;
+`;
