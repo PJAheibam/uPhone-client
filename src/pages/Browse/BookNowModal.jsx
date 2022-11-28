5c0cec005ac03f5b5d927e7661495e10140d6b9b
PJAheibam <prosenjitsingha28@gmail.com>
2022-11-29 01:04:07 +0600

    book now title added

--- src/pages/Browse/BookNowModal.jsx
+++ src/pages/Browse/BookNowModal.jsx
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Input, InputWrapper, Label } from "../../components/formItems";
 import Modal from "../../components/Modal";
 import { GradientButton } from "../../components/Button";
+import { Pagination, Navigation } from "swiper";
 
 function BookNowModal({ product, user, setProduct }) {
   function handleModalClose() {
@@ -15,7 +16,21 @@ function BookNowModal({ product, user, setProduct }) {
       <Wrapper>
         <Heading>Book Now</Heading>
         <MobileSection>
-          {/* <SwiperContainer></SwiperContainer> */}
+          <SwiperContainer
+            // pagination={{
+            //   type: "fraction",
+            // }}
+            navigation={true}
+            modules={[Pagination, Navigation]}
+          >
+            {product?.images &&
+              product?.images.map((image, i) => (
+                <SwiperItem key={i}>
+                  <img src={image?.display_url} alt={image?.title} />
+                </SwiperItem>
+              ))}
+          </SwiperContainer>
+          <Title>{product?.name}</Title>
         </MobileSection>
 
         {/******************* Form Start  *********************/}
@@ -48,9 +63,22 @@ const Heading = styled.h1`
   text-transform: capitalize;
 `;
 
-const SwiperContainer = styled(Swiper)``;
+const SwiperContainer = styled(Swiper)`
+  max-width: 300px;
+  border-radius: var(--border-radius-md);
+`;
 
-const SwiperItem = styled(SwiperSlide)``;
+const SwiperItem = styled(SwiperSlide)`
+  width: 200px;
+  height: 180px;
+  overflow: hidden;
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+    object-position: center;
+  }
+`;
 
 const Form = styled.form`
   display: flex;
@@ -59,3 +87,5 @@ const Form = styled.form`
 `;
 
 const MobileSection = styled.div``;
+
+const Title = styled.h2``;
